a2ed5834e09c3dd32a4f7b52ba6db2f41694d5e8
mohi <mohisadeghi302@gmail.com>
2023-04-13 12:59:32 +0430

    Change requirements

--- person.rb
+++ person.rb
@@ -9,13 +9,14 @@ class Person
     @parent_permission = parent_permission
   end
 
-  def can_use_services?
-    @age >= 18 || @parent_permission
-  end
-
   private
 
   def of_age?
     @age >= 18
   end
+
+  def can_use_services?
+   of_age?() || @parent_permission
+  end
+
 end
