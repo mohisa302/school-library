e9fd1fd7b8057876a9699175eed29b9832f79bcb
Mohi <MOHISADEGHI302@GMAIL.com>
2023-04-13 13:58:38 +0330

    Fix last merge

--- person.rb
+++ person.rb
@@ -12,6 +12,10 @@ class Person < Nameable
     @parent_permission = parent_permission
   end
 
+  def correct_name
+    @name
+  end
+  
   private
 
   def of_age?
@@ -19,6 +23,6 @@ class Person < Nameable
   end
 
   def can_use_services?
-    of_age? || @parent_permission
+    of_age ()? || @parent_permission
   end
 end
