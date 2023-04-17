96dc0d88757d7e74b538001726a4f6af1ee0229c
Mohi <MOHISADEGHI302@GMAIL.com>
2023-04-17 11:25:57 +0330

    Add main func

--- main.rb
+++ main.rb
@@ -2,12 +2,15 @@ require_relative './app'
 require_relative './library'
 require_relative './menu'
 
-library = Library.new
-app = App.new(library)
-menu = Menu.new(app)
 
+def main
+  library = Library.new
+  app = App.new(library)
+  menu = Menu.new(app)
+  menu.show
+end
 puts 'Hello here! welcome to School Library App!'
 puts ''
 
-menu.show
+main
 
