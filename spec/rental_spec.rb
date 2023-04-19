704d05431da740badd16b97feb212ae15d9c16b0
Mohi <MOHISADEGHI302@GMAIL.com>
2023-04-19 14:48:20 +0330

    Finish rental class

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -2,3 +2,16 @@ require_relative 'spec_helper'
 require_relative '../student'
 require_relative '../rental'
 require_relative '../book'
+
+describe Classroom do
+  before :each do
+    book = Book.new('Cry', 'John')
+    person = Student.new('4A', 17, 'John', parent_permission: true)
+    @rental = Rental.new(book, person, '2022-10-2')
+  end
+
+  it 'takes three parameters and returns a Rental object' do
+    expect(@rental).to be_an_instance_of(Rental)
+  end
+
+end
